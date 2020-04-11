275d40fd13e206b130958c85fca62c4d8ed257f4
drimailorr <drimailorr@gmail.com>
2020-04-11 22:44:39 +0300

    initial

---
+++ src/actions/index.js
@@ -0,0 +1,10 @@
+import jsonPlaceholder from '../apis/jsonPlaceholder'
+
+const fetchPosts = () => async dispatch => {
+    const response = await jsonPlaceholder.get('/posts')
+
+    dispatch({ type: 'FETCH_POSTS', payload: response.data })
+
+}
+
+export default fetchPosts
